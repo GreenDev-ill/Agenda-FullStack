4ba534f8e8f62db23797034528eb037698f067c5
Ronan <ronan.bottcher@gmail.com>
2025-11-28 21:51:50 -0300

    render-v9/axios

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://agenda-fullstack-qyic.onrender.com"
+    baseURL: "http://agenda-fullstack-qyic.onrender.com"
 })
 
 api.interceptors.request.use(
